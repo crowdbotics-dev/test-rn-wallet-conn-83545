6247c97c057885b7088f1432116fb3a7c235d9cf
abdelhak@crowdbotics.com <abdelhak@crowdbotics.com>
2023-05-16 17:54:36 +0100

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,11 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "test-rn-wallet-conn-83545",
-  url: "https://test-rn-wallet-conn-83545.botics.co",
-  api: "https://test-rn-wallet-conn-83545.botics.co/api/v1"
-}
+  "name": "test-rn-wallet-conn-83545",
+  "url": "https://test-rn-wallet-conn-83545.botics.co",
+  "api": "https://test-rn-wallet-conn-83545.botics.co/api/v1",
+  "initialRoute": "Untitled1"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
